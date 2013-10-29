1ca47f52bf4e5b848913b7fb3339906919d9061e
vamshis <vamshis@qhode.com>
2013-10-29 17:22:38 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,51 @@
+#mySql build sample
+import MySQLdb
+
+db = MySQLdb.connect("localhost","root","qhode25")
+cursor = db.cursor()
+cursor.execute("DROP DATABASE IF EXISTS TESTDB")
+
+cursor.execute("CREATE DATABASE TESTDB")
+
+cursor.execute("USE TESTDB")
+
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+
+#creating TABLE
+sql1 = """CREATE TABLE EMPLOYEE (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,  
+         SEX CHAR(1),
+         INCOME FLOAT )"""
+cursor.execute(sql1)
+
+#inserting VALUES
+sql2 = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   cursor.execute(sql2)
+   db.commit()
+except:
+   db.rollback()
+
+
+# Query
+sql3 = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > '%d'" % (1000)
+try:
+   cursor.execute(sql3)
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[0]
+      lname = row[1]
+      age = row[2]
+      sex = row[3]
+      income = row[4]
+      print "fname=%s,lname=%s,age=%d,sex=%s,income=%d" % \
+             (fname, lname, age, sex, income )
+except:
+   print "Error: unable to fecth data"
+   
+db.close()
